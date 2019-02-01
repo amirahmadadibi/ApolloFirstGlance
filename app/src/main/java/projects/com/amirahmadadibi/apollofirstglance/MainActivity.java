ff9009ac465252d173957c33789dfeb4a0384eb7
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2019-02-01 14:58:24 +0330

    first query using graphql

--- app/src/main/java/projects/com/amirahmadadibi/apollofirstglance/MainActivity.java
+++ app/src/main/java/projects/com/amirahmadadibi/apollofirstglance/MainActivity.java
@@ -2,12 +2,42 @@ package projects.com.amirahmadadibi.apollofirstglance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.jetbrains.annotations.NotNull;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    ApolloClient apolloClient;
+    public static final String BASE_URL = "https://api.graph.cool/simple/v1/cjrhtrvlvari401295bq0ul6f";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        apolloClient = ApolloClient.builder()
+                .serverUrl(BASE_URL)
+                .okHttpClient(okHttpClient)
+                .build();
+        apolloClient.query(
+                AllPostsQuery.builder()
+                        .build()
+        ).enqueue(new ApolloCall.Callback<AllPostsQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<AllPostsQuery.Data> response) {
+                Log.d("tag", "onResponse: " + response.data().allPosts.get(0));
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+
+            }
+        });
     }
 }
